87414b0a97b2057a22e10c9cdb878f16398312ad
Awwdil <adil.khalil76@gmail.com>
2024-07-26 16:59:58 +0500

    Updates

---
+++ precache-manifest.219b63bf45422d062f50511f7adb5510.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cbaa81fefaca94d1003089743db390e",
+    "url": "/new-io-latest-adil/index.html"
+  },
+  {
+    "revision": "7be04bef01b17dd47c1a",
+    "url": "/new-io-latest-adil/static/css/2.98036eff.chunk.css"
+  },
+  {
+    "revision": "1c2f7f5cbc4ffadbd3ea",
+    "url": "/new-io-latest-adil/static/css/main.d90ceccc.chunk.css"
+  },
+  {
+    "revision": "7be04bef01b17dd47c1a",
+    "url": "/new-io-latest-adil/static/js/2.bc06a38b.chunk.js"
+  },
+  {
+    "revision": "9ecbc517f060d70429dd4108158713c8",
+    "url": "/new-io-latest-adil/static/js/2.bc06a38b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c2f7f5cbc4ffadbd3ea",
+    "url": "/new-io-latest-adil/static/js/main.e4edc456.chunk.js"
+  },
+  {
+    "revision": "9e4bdf2f5193ebcea78e",
+    "url": "/new-io-latest-adil/static/js/runtime-main.e9b311ca.js"
+  },
+  {
+    "revision": "371c08b8f759579d7a53f9e4df6a083e",
+    "url": "/new-io-latest-adil/static/media/fbVideo.371c08b8.png"
+  },
+  {
+    "revision": "643ff345e4de1b6d2efbc0af7735b459",
+    "url": "/new-io-latest-adil/static/media/mainlogo.643ff345.png"
+  },
+  {
+    "revision": "d7761a9626ab49a70f135301b217a4e1",
+    "url": "/new-io-latest-adil/static/media/meta.d7761a96.svg"
+  },
+  {
+    "revision": "68dbcb72db34d725529f3cce3f879088",
+    "url": "/new-io-latest-adil/static/media/metalogo.68dbcb72.png"
+  },
+  {
+    "revision": "e03f40f14d34e08726f787848dfd7401",
+    "url": "/new-io-latest-adil/static/media/thankstop.e03f40f1.png"
+  }
+]);
